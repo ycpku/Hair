e2e4536f386ecc8f0ddbf0d45a6ebb5d04b987a7
Cheng Yu <1537772987@qq.com>
2023-12-03 11:54:45 +0800

    add more profiler

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import taichi as ti
 import argparse
 from scene import Scene
 from der import Simulator
+import time
 
 if __name__=="__main__":
+    total0 = time.perf_counter()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--scene', type=str, help='XML scene file to load')
     parser.add_argument('-o', '--outfile', type=str, help='Readable file to save simulation state to')
@@ -14,6 +17,8 @@ if __name__=="__main__":
     # arch, default_fp = ti.gpu, ti.f32
     ti.init(arch=arch, default_fp=default_fp, debug=False, kernel_profiler=True)
     args = parser.parse_args()
+
+    t1 = time.perf_counter()
 
     window = ti.ui.Window("Hair DER", (1024, 1024), vsync=True)
 
@@ -28,13 +33,15 @@ if __name__=="__main__":
     sim = Simulator(scene.n_rods, scene.n_vertices, scene.params, time_step, default_fp)
     sim.initialize(scene.x, scene.is_fixed, scene.v)
 
+    t2 = time.perf_counter()
+    print(f"Initialization time: {t2-t1:.6f} seconds") # time for initialization
+
     frames = 0
     file = open('outfile.txt', 'w')
     sim.write_to_file(file, frames)
 
     canvas = window.get_canvas()
     canvas.set_background_color((1, 1, 1))
-
     while window.running and frames < duration*framerate:
         for _ in range(int(1e-3//time_step)):
             # sim.explicit_integrator()
@@ -52,5 +59,8 @@ if __name__=="__main__":
     if arch==ti.gpu:
         ti.sync()
     ti.profiler.print_kernel_profiler_info()
-    print(f"Real time: {sim.t_solve:.6f} seconds") # time for solving matrix
+    print(f"Matrix solving time: {sim.t_solve:.6f} seconds") # time for solving matrix
     file.close()
+
+    total1 = time.perf_counter()
+    print(f"Total execution time: {sim.t_solve:.6f} seconds")
